21a250a5046377dcfbed6f82b2044eac9f1ed257
fihagjer <hans.ole.gjerdrum@finn.no>
2013-12-18 11:58:12 +0100

    simpleDigit

--- src/test/java/kata/ExampleTest.java
+++ src/test/java/kata/ExampleTest.java
@@ -19,5 +19,20 @@ public final class ExampleTest {
     public void exampleUsingFestAssert() {
         org.fest.assertions.Assertions.assertThat(Example.sum(1,2,3)).isEqualTo(6);
     }
+
+    @Test
+    public void nullIsNull() {
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(0)).isEqualTo("null");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(1)).isEqualTo("en");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(2)).isEqualTo("to");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(3)).isEqualTo("tre");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(4)).isEqualTo("fire");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(5)).isEqualTo("fem");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(6)).isEqualTo("seks");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(7)).isEqualTo("sju");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(8)).isEqualTo("åtte");
+        org.fest.assertions.Assertions.assertThat(Example.singleDigit(9)).isEqualTo("ni");
+
+    }
     
 }
