19cb3c5e6607b1380a4591dad559e73b038b8413
filojoha <lotte.johansen@finn.no>
2013-12-18 12:14:57 +0100

    fix firstPosition. create secondPositon

--- src/main/java/kata/Example.java
+++ src/main/java/kata/Example.java
@@ -12,4 +12,21 @@ public final class Example {
         return result;
     }
 
+    public static String firstPosition(int number)  {
+        switch (number) {
+            case 0 : return "null";
+            case 1 : return "en";
+            case 2 : return "to";
+            case 3 : return "tre";
+            case 4 : return "fire";
+            case 5 : return "fem";
+            case 6 : return "seks";
+            case 7 : return "sju";
+            case 8 : return "åtte";
+            case 9 : return "ni";
+            default : return "";
+            }
+    }
+
+
 }
--- src/test/java/kata/ExampleTest.java
+++ src/test/java/kata/ExampleTest.java
@@ -1,7 +1,8 @@
 package kata;
 
-import kata.Example;
 import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
 
 public final class ExampleTest {
     
@@ -21,18 +22,29 @@ public final class ExampleTest {
     }
 
     @Test
-    public void nullIsNull() {
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(0)).isEqualTo("null");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(1)).isEqualTo("en");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(2)).isEqualTo("to");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(3)).isEqualTo("tre");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(4)).isEqualTo("fire");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(5)).isEqualTo("fem");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(6)).isEqualTo("seks");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(7)).isEqualTo("sju");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(8)).isEqualTo("åtte");
-        org.fest.assertions.Assertions.assertThat(Example.singleDigit(9)).isEqualTo("ni");
-
+    public void oneDigits() {
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(0)).isEqualTo("null");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(1)).isEqualTo("en");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(2)).isEqualTo("to");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(3)).isEqualTo("tre");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(4)).isEqualTo("fire");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(5)).isEqualTo("fem");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(6)).isEqualTo("seks");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(7)).isEqualTo("sju");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(8)).isEqualTo("åtte");
+        org.fest.assertions.Assertions.assertThat(Example.firstPosition(9)).isEqualTo("ni");
     }
-    
+
+    @Test
+    public void twoDigits() {
+        assertThat(Example.secondPosition(1).isEqualTo("ti"));
+        assertThat(Example.secondPosition(2).isEqualTo("tjue"));
+        assertThat(Example.secondPosition(3).isEqualTo("tretti"));
+        assertThat(Example.secondPosition(4).isEqualTo("førti"));
+        assertThat(Example.secondPosition(5).isEqualTo("femti"));
+        assertThat(Example.secondPosition(6).isEqualTo("seksti"));
+        assertThat(Example.secondPosition(7).isEqualTo("søtti"));
+        assertThat(Example.secondPosition(8).isEqualTo("åtti"));
+        assertThat(Example.secondPosition(9).isEqualTo("nitti"));
+    }
 }
